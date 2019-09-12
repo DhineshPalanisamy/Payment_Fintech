bd98f7f1e091bf601c33483d0168b76a2f7def03
quantumofcosmos <quantumofcosmos@gmail.com>
2019-09-12 12:44:02 +0530

    main for 5 screens

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ import tornado.autoreload
 # When running this app on the local machine, default the port to 8000
 port = int(os.getenv('PORT', 8000))
 
+class landingPage(tornado.web.RequestHandler):
+    def get(self):
+        self.render("static/indexx.html")
+
 class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("static/register.html")
 
 class regRequ(tornado.web.RequestHandler):
     def post(self):
-        base_url = 'https://192.86.33.94:19443/cusregapi/AccountNo?acctno='
+        base_url = 'https://192.86.33.94:19443/cbs/cusreg?AcctNo='
         # 100000001001 is the only working answer
         headers = {'Content-Type': 'application/json'}
         end_url= base_url+str(self.get_body_argument("accnt"))
@@ -25,34 +29,79 @@ class regRequ(tornado.web.RequestHandler):
 
 class basicDeRequestHandler(tornado.web.RequestHandler):
     def get(self):
-        #req = requests.get(base_url, headers=headers, auth=('ibmuser', 'ibmuser'), verify=False)
-        # self.write("main_modified")
         self.render("static/deregister.html")
 
-class resourceRequestHandler(tornado.web.RequestHandler):
-    def get(self, id):
-        req = requests.get(base_url, headers=headers, auth=('ibmuser', 'ibmuser'), verify=False)
-        self.write("Querying tweet with id " + str(req.content))
+class deRegRequ(tornado.web.RequestHandler):
+    def post(self):
+        # base_url = 'https://192.86.33.94:19443/cusdereg/AccountNo?acctno='
+        base_url = 'https://192.86.33.94:19443/cbscs/cusdereg?AcctNo='
+        # 100000001001 is the only working answer
+        headers = {'Content-Type': 'application/json'}
+        end_url= base_url+str(self.get_body_argument("accnt"))
+        req = requests.get(end_url, headers=headers, auth=('ibmuser', 'ibmuser'), verify=False)
+        json_out = req.json()
+        self.render("static/genericresp.html",msg=json_out['CSDGRES']['CSRGRES']['MESSAGES'])
 
-class queryStringRequestHandler(tornado.web.RequestHandler):
+class basicPayHandler(tornado.web.RequestHandler):
     def get(self):
-        n = int(self.get_argument("n"))
-        r = "odd" if n % 2 else "even"
+        self.render("static/pay.html")
 
-        self.write("the number " + str(n) + " is " + r)
+class payRequ(tornado.web.RequestHandler):
+    def post(self):
+        # base_url = 'https://192.86.33.94:19443/cusdereg/AccountNo?acctno='
+        base_url = 'https://192.86.33.94:19443/cbscs/cusdereg?AcctNo='
+        # 100000001001 is the only working answer
+        headers = {'Content-Type': 'application/json'}
+        end_url= base_url+str(self.get_body_argument("accnt"))
+        req = requests.get(end_url, headers=headers, auth=('ibmuser', 'ibmuser'), verify=False)
+        json_out = req.json()
+        self.render("static/genericresp.html",msg=json_out['CSDGRES']['CSRGRES']['MESSAGES'])
 
-class staticRequestHandler(tornado.web.RequestHandler):
+class basicRevHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("static/genericresp.html",msg="Something generic")
+        self.render("static/pay.html")
+
+class revRequ(tornado.web.RequestHandler):
+    def post(self):
+        # base_url = 'https://192.86.33.94:19443/cusdereg/AccountNo?acctno='
+        base_url = 'https://192.86.33.94:19443/cbscs/cusdereg?AcctNo='
+        # 100000001001 is the only working answer
+        headers = {'Content-Type': 'application/json'}
+        end_url= base_url+str(self.get_body_argument("accnt"))
+        req = requests.get(end_url, headers=headers, auth=('ibmuser', 'ibmuser'), verify=False)
+        json_out = req.json()
+        self.render("static/genericresp.html",msg=json_out['CSDGRES']['CSRGRES']['MESSAGES'])
+
+# class resourceRequestHandler(tornado.web.RequestHandler):
+#     def get(self, id):
+#         req = requests.get(base_url, headers=headers, auth=('ibmuser', 'ibmuser'), verify=False)
+#         self.write("Querying tweet with id " + str(req.content))
+#
+# class queryStringRequestHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         n = int(self.get_argument("n"))
+#         r = "odd" if n % 2 else "even"
+#
+#         self.write("the number " + str(n) + " is " + r)
+#
+# class staticRequestHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         self.render("static/genericresp.html",msg="Something generic")
 
 if __name__ == "__main__":
     app = tornado.web.Application([
-        (r"/", basicRequestHandler),
+        (r"/", landingPage),
+        (r"/register", basicRequestHandler),
+        (r"/regrequ", regRequ),
         (r"/deregister", basicDeRequestHandler),
-        (r"/blog", staticRequestHandler),
-        (r"/isEven", queryStringRequestHandler),
-        (r"/tweet/([0-9]+)", resourceRequestHandler),
-        (r"/regRequ", regRequ)
+        (r"/deregrequ", deRegRequ),
+        (r"/paymentauth", basicPayHandler),
+        (r"/payrequ", payRequ),
+        (r"/paymentrevauth", basicRevHandler),
+        (r"/payrevrequ", revRequ),
+        # (r"/blog", staticRequestHandler),
+        # (r"/isEven", queryStringRequestHandler),
+        # (r"/tweet/([0-9]+)", resourceRequestHandler),
     ])
 
     app.listen(port)
