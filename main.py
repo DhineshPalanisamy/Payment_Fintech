e540ca928a37fbdca198566af924d1086f2a0a23
Vishnupriya <priyavp666@gmail.com>
2019-09-13 10:25:05 +0000

    reversal url corrected

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ class revRequ(tornado.web.RequestHandler):
         base_url = 'https://192.86.33.94:19443/cuspymtrev/cuspayrev?acctono='
         # 100000001001 is the only working answer
         headers = {'Content-Type': 'application/json'}
-        end_url= base_url+str(self.get_body_argument("accnt"))+"&tranid="++str(self.get_body_argument("trans"))+"&revamt="++str(self.get_body_argument("debit_amt"))
+        end_url= base_url+str(self.get_body_argument("accnt"))+"&tranid="+str(self.get_body_argument("trans"))+"&revamt="+str(self.get_body_argument("debit_amt"))
         req = requests.get(end_url, headers=headers, auth=('ibmuser', 'ibmuser'), verify=False)
         json_out = req.json()
         self.render("static/genericresp.html",msg=json_out['CSREVRES']['CSREVRES']['MESSAGES'],cname=json_out['CSREVRES']['CSREVRES']['CUSTOMER_NAME'],hbal=json_out['CSREVRES']['CSREVRES']['HOLD_BALANCE'],lbal=json_out['CSREVRES']['CSREVRES']['LEDGER_BL'],bal=json_out['CSREVRES']['CSREVRES']['AVAILABLE_BALANCE'],cid=json_out['CSREVRES']['CSREVRES']['CUSTOMER_ID'],credamt=json_out['CSREVRES']['CSREVRES']['CREDIT_AMOUNT_RES'],tid=json_out['CSREVRES']['CSREVRES']['TRANSACTIONS_ID'],date=json_out['CSREVRES']['CSREVRES']['SYS_DATE'],time=json_out['CSREVRES']['CSREVRES']['SYS_TIME'],bloc="payrev")
