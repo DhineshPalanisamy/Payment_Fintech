5fb47cd0572cd8872b3c9a5efa4aff8396fd8159
PriyaVP-crypto <56911834+PriyaVP-crypto@users.noreply.github.com>
2019-11-01 14:51:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import tornado.autoreload
 # On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8000
 port = int(os.getenv('PORT', 8000))
+host=int(os.getenv('IP',0.0.0.0))
 
 class landingPage(tornado.web.RequestHandler):
     def get(self):
@@ -92,5 +93,6 @@ if __name__ == "__main__":
     # TODO remove in prod
     tornado.autoreload.start()
     print(port)
+    print(host)
     print("I'm listening on port specified")
     tornado.ioloop.IOLoop.current().start()
